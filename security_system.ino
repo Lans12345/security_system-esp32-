f52f982e252c5eaccefc654afa31ad1e27018df5
Lans12345 <olanalans12345@gmail.com>
2023-04-08 19:49:34 +0800

    initial code

--- security_system.ino
+++ security_system.ino
@@ -7,6 +7,7 @@
 #define BUZZER_PIN 12
 #define RELAY_PIN 13
 
+
 const char* ssid = "your_SSID";
 const char* password = "your_PASSWORD";
 
@@ -18,10 +19,29 @@ const char* smtpPassword = "123moviestf";
 
 bool motionDetected = false;
 
-ESP32_MailClient MailClient;
+// Camera pins for ESP32-CAM AI-Thinker
+#define CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+// Email data
 SMTPData smtpData;
-const char* emailSubject = "Motion detected!";
-const char* emailMessage = "Motion has been detected by NodeMCU ESP32.";
+String emailSubject = "ESP32-CAM Motion Detected";
+String emailMessage = "Motion has been detected! See attached image.";
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
@@ -31,51 +51,131 @@ void setup() {
   digitalWrite(RELAY_PIN, LOW);
   Serial.begin(115200);
 
+  // Start the camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to Wi-Fi...");
+}
+Serial.println("Connected to Wi-Fi");
+
+// Connect to camera
+camera_config_t config;
+config.ledc_channel = LEDC_CHANNEL_0;
+config.ledc_timer = LEDC_TIMER_0;
+config.pin_d0 = 5;
+config.pin_d1 = 18;
+config.pin_d2 = 19;
+config.pin_d3 = 21;
+config.pin_d4 = 36;
+config.pin_d5 = 39;
+config.pin_d6 = 34;
+config.pin_d7 = 35;
+config.pin_xclk = 0;
+config.pin_pclk = 22;
+config.pin_vsync = 25;
+config.pin_href = 23;
+config.pin_sscb_sda = 26;
+config.pin_sscb_scl = 27;
+config.pin_pwdn = 32;
+config.pin_reset = -1;
+config.xclk_freq_hz = 20000000;
+config.pixel_format = PIXFORMAT_JPEG;
+if(psramFound()){
+config.frame_size = FRAMESIZE_UXGA;
+config.jpeg_quality = 10;
+config.fb_count = 2;
+} else {
+config.frame_size = FRAMESIZE_SVGA;
+config.jpeg_quality = 12;
+config.fb_count = 1;
+}
+
+esp_err_t err = esp_camera_init(&config);
+if (err != ESP_OK) {
+Serial.printf("Camera init failed with error 0x%x", err);
+return;
+}
+}
+
+void loop() {
+if (digitalRead(PIR_PIN) == HIGH) {
+if (!motionDetected) {
+Serial.println("Motion detected!");
+motionDetected = true;
+digitalWrite(BUZZER_PIN, HIGH);
+digitalWrite(RELAY_PIN, LOW);
+delay(1000);
+digitalWrite(BUZZER_PIN, LOW);
+digitalWrite(RELAY_PIN, HIGH);
+  // Capture an image
+  camera_fb_t* fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Failed to capture image");
+    return;
   }
-  Serial.println("Connected to Wi-Fi");
 
   // Set the SMTP Server Email data
   smtpData.setLogin(smtpServer, 465, smtpUser, smtpPassword);
   smtpData.setSender("ESP32", emailSender);
   smtpData.setPriority(0);
   smtpData.setSubject(emailSubject);
+  smtpData.addAttachment("image.jpg", fb->buf, fb->len);
   smtpData.setMessage(emailMessage, false);
   smtpData.setRecipient(emailRecipient);
   smtpData.setSendCallback(sendCallback);
-}
 
-void loop() {
-  if (digitalRead(PIR_PIN) == HIGH) {
-    if (!motionDetected) {
-      Serial.println("Motion detected!");
-      motionDetected = true;
-      digitalWrite(BUZZER_PIN, HIGH);
-      digitalWrite(RELAY_PIN, LOW);
-      delay(1000);
-      digitalWrite(BUZZER_PIN, LOW);
-      digitalWrite(RELAY_PIN, HIGH);
-
-      // Send the email
-      if (!MailClient.sendMail(smtpData)) {
-        Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
-      }
-
-      // Clear all data from the SMTP object
-      smtpData.empty();
-    }
-  } else {
-    if (motionDetected) {
-      Serial.println("No motion detected.");
-      motionDetected = false;
-    }
+  // Send the email
+  if (!MailClient.sendMail(smtpData)) {
+    Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
   }
+
+  // Clear all data from the SMTP object
+  smtpData.empty();
+
+  // Delete the image buffer
+  esp_camera_fb_return(fb);
+}} else {
+if (motionDetected) {
+Serial.println("No motion detected.");
+motionDetected = false;
+}
+}
 }
 
 void sendCallback(SendStatus msg) {
-  // Print the status of the email sending
-  Serial.println(msg.info());
+// Print the status of the email sending
+Serial.println(msg.info());
 }
+
