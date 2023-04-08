7370b1f849e23827bc72bc1a1c27de659670ee37
Lans12345 <olanalans12345@gmail.com>
2023-04-08 19:35:26 +0800

    wip email func

--- security_system.ino
+++ security_system.ino
@@ -1,23 +1,26 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include "esp_camera.h"
+#include <WiFiClientSecure.h>
+#include <Base64.h>
+#include <ESP32Mail.h>
 
 #define PIR_PIN 14
 #define BUZZER_PIN 12
 #define RELAY_PIN 13
 
-
-const char* ssid = "4ac7cf";
-const char* password = "260547987";
-const char* smtp_server = "smtp.gmail.com";
-const int smtp_port = 465; // Gmail SMTP port for SSL
-const char* email_from = "olanalans12345@gmail.com";
-const char* email_password = "123moviestf";
-const char* email_to = "olanalans12345@gmail.com";
-const char* email_subject = "Test email from ESP32";
-const char* email_body = "This is a test email sent from an ESP32.";
-
 bool motionDetected = false;
+
+const char* ssid = "your_SSID";
+const char* password = "your_PASSWORD";
+
+
+
+// Replace with your email credentials
+const char* smtp_host = "smtp.gmail.com";
+const int smtp_port = 465;
+const char* email_address = "olanalans12345@gmail.com";
+const char* email_password = "123moviestf";
 
 void setup() {
   pinMode(PIR_PIN, INPUT);
@@ -26,11 +29,18 @@ void setup() {
   digitalWrite(BUZZER_PIN, LOW);
   digitalWrite(RELAY_PIN, LOW);
   Serial.begin(9600);
+
+ 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("WiFi connected");
+
 }
 
 void loop() {
-
-  SMTPClient smtp(smtp_server, smtp_port);
   if (digitalRead(PIR_PIN) == HIGH) {
     if (!motionDetected) {
       Serial.println("Motion detected!");
@@ -41,34 +51,20 @@ void loop() {
       digitalWrite(BUZZER_PIN, LOW);
       digitalWrite(RELAY_PIN, HIGH);
 
-        if (!smtp.connect()) {
-    Serial.println("Failed to connect to SMTP server.");
-    return;
-  }
-  
-  // Login to the SMTP server
-  if (!smtp.login(email_from, email_password)) {
-    Serial.println("Failed to login to SMTP server.");
-    smtp.quit();
-    return;
-  }
-  
-  // Create the email message
-  EmailMessage email;
-  email.setSubject(email_subject);
-  email.setSender(email_from);
-  email.addRecipient(email_to);
-  email.setText(email_body);
-  
-  // Send the email
-  if (!smtp.send(email)) {
-    Serial.println("Failed to send email.");
-    smtp.quit();
-    return;
-  }
-  
-  Serial.println("Email sent successfully.");
-  smtp.quit();
+           // Replace with your recipient email address
+        String recipient = "olanalans12345@gmail.com";
+        String subject = "Hello from Arduino";
+        String message = "This is a test email sent from Arduino using ESP8266.";
+
+        if (smtpClient.send(recipient, subject, message))
+        {
+            Serial.println("Email sent successfully!");
+        }
+        else
+        {
+            Serial.println("Error sending email.");
+        }
+
     }
   } else {
     if (motionDetected) {
